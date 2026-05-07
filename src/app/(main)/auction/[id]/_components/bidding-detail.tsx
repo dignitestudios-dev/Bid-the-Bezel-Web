0626d72b4e813fccc4b719c124a8270cd619a0b1
huzaifa-koder <huzaifa.tanveer@launchbox.pk>
2026-05-07 16:47:57 +0500

    push

--- src/app/(main)/auction/[id]/_components/bidding-detail.tsx
+++ src/app/(main)/auction/[id]/_components/bidding-detail.tsx
@@ -24,8 +24,9 @@ const BiddingDetail = ({ product }: Props) => {
   const { data: bidsData, isLoading: bidsLoading } = useGetProductBids(product?._id, 1, 10);
   const { data: paginatedBids, isLoading: paginatedBidsLoading } = useGetProductBids(product?._id, currentPage, 10);
 
-  const noBids = !bidsLoading && (bidsData?.data?.bids.length ?? 0) === 0;
+  const noBids = !bidsLoading && (bidsData?.data?.bids?.length ?? 0) === 0;
 
+  
   if (noBids) {
     queryClient.invalidateQueries({ queryKey: ["get-listing-detail", product?._id] });
   }
